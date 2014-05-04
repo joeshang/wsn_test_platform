45fdd9696e0ac21744c65d08c1c9727e56a4fdb7
joeshang <shangchuanren@gmail.com>
2014-05-04 13:17:51 +0800

    [Feature] 将Led驱动修改成GPIO驱动，加入与FPGA交互相关的GPIO控制，并修正cmd，使其更加可靠

---
+++ drivers/gpio/oint_gpio.c
@@ -0,0 +1,210 @@
+/**
+ * @file oint_gpio.c
+ * @brief OINT中GPIO的驱动
+ * @author Joe Shang <shangchuanren@gmail.com>
+ * @version 
+ * @date 2014-04-30
+ */
+
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/init.h>
+#include <linux/errno.h>
+#include <linux/ioctl.h>
+#include <linux/fs.h>
+#include <linux/types.h>
+#include <linux/miscdevice.h>
+#include <linux/cdev.h>
+#include <asm/uaccess.h>
+
+#include <mach/gpio.h>
+#include <mach/regs-gpio.h>
+#include <plat/gpio-cfg.h>
+
+#include "oint_gpio.h"
+
+#define GPIO_NUM        ARRAY_SIZE(oint_gpios) 
+#define DEVICE_NAME     "oint_gpio"
+
+static int oint_gpios[] = {
+    // red leds
+    S3C64XX_GPE(1), // red s1
+    S3C64XX_GPE(3), // red s2
+    S3C64XX_GPM(0), // red s3
+    S3C64XX_GPM(2), // red s4
+    S3C64XX_GPM(4), // red s5
+    S3C64XX_GPQ(1), // red s6
+    S3C64XX_GPQ(3), // red s7
+    S3C64XX_GPQ(5), // red s8
+    // green leds
+    S3C64XX_GPE(2), // green s1
+    S3C64XX_GPE(4), // green s2
+    S3C64XX_GPM(1), // green s3
+    S3C64XX_GPM(3), // green s4
+    S3C64XX_GPM(5), // green s5
+    S3C64XX_GPQ(2), // green s6
+    S3C64XX_GPQ(4), // green s7
+    S3C64XX_GPQ(6), // green s8
+    // current sample switch
+    S3C64XX_GPN(9),
+    // fpga reset
+    S3C64XX_GPN(11),
+    // fpga SDRAM status
+    S3C64XX_GPL(8),
+};
+
+static long oint_gpio_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
+{
+    int ret = 0;
+    int gpio_index = 0;
+    int err = 0;
+    int input;
+
+    if (_IOC_TYPE(cmd) != OINT_GPIO_IOC_MAGIC)
+    {
+        return -EINVAL;
+    }
+    if (_IOC_NR(cmd) > GPIO_NUM)
+    {
+        return -EINVAL;
+    }
+
+    if (_IOC_DIR(cmd) & _IOC_READ)
+    {
+        err = !access_ok(VERIFY_WRITE, (void *)arg, _IOC_SIZE(cmd));
+    }
+    else if (_IOC_DIR(cmd) & _IOC_READ)
+    {
+        err = !access_ok(VERIFY_READ, (void *)arg, _IOC_SIZE(cmd));
+    }
+    if (err)
+    {
+        return -EFAULT;
+    }
+
+    gpio_index = _IOC_NR(cmd);
+    switch (cmd)
+    {
+        case OINT_GPIO_IOC_LED_RED_S1:
+        case OINT_GPIO_IOC_LED_RED_S2:
+        case OINT_GPIO_IOC_LED_RED_S3:
+        case OINT_GPIO_IOC_LED_RED_S4:
+        case OINT_GPIO_IOC_LED_RED_S5:
+        case OINT_GPIO_IOC_LED_RED_S6:
+        case OINT_GPIO_IOC_LED_RED_S7:
+        case OINT_GPIO_IOC_LED_RED_S8:
+        case OINT_GPIO_IOC_LED_GREEN_S1:
+        case OINT_GPIO_IOC_LED_GREEN_S2:
+        case OINT_GPIO_IOC_LED_GREEN_S3:
+        case OINT_GPIO_IOC_LED_GREEN_S4:
+        case OINT_GPIO_IOC_LED_GREEN_S5:
+        case OINT_GPIO_IOC_LED_GREEN_S6:
+        case OINT_GPIO_IOC_LED_GREEN_S7:
+        case OINT_GPIO_IOC_LED_GREEN_S8:
+        case OINT_GPIO_IOC_CURR_SWITCH:
+        case OINT_GPIO_IOC_FPGA_RESET:
+            gpio_set_value(oint_gpios[gpio_index], arg);
+            break;
+        case OINT_GPIO_IOC_FPGA_SDRAM_STATUS:
+            input = gpio_get_value(oint_gpios[gpio_index]);
+            put_user(input, (int *)arg);
+            break;
+        default:
+           return -EINVAL; 
+    }
+
+    return ret;
+}
+
+static struct file_operations oint_gpio_fops = {
+    .owner          = THIS_MODULE,
+    .unlocked_ioctl = oint_gpio_ioctl
+};
+
+static struct miscdevice oint_gpio_dev = {
+    .minor          = MISC_DYNAMIC_MINOR,
+    .name           = DEVICE_NAME,
+    .fops           = &oint_gpio_fops
+};
+
+static int __init oint_gpio_init(void)
+{
+    int ret;
+    int i;
+
+    /* init gpio */
+    for (i = 0; i < GPIO_NUM; i++)
+    {
+        ret = gpio_request(oint_gpios[i], "OINT GPIO");
+        if (ret)
+        {
+            printk("%s: request GPIO %d failed, ret = %d\n", DEVICE_NAME, oint_gpios[i], ret);
+            return ret;
+        }
+
+        switch (i)
+        {
+            case OINT_LED_RED_S1:
+            case OINT_LED_RED_S2:
+            case OINT_LED_RED_S3:
+            case OINT_LED_RED_S4:
+            case OINT_LED_RED_S5:
+            case OINT_LED_RED_S6:
+            case OINT_LED_RED_S7:
+            case OINT_LED_RED_S8:
+            case OINT_LED_GREEN_S1:
+            case OINT_LED_GREEN_S2:
+            case OINT_LED_GREEN_S3:
+            case OINT_LED_GREEN_S4:
+            case OINT_LED_GREEN_S5:
+            case OINT_LED_GREEN_S6:
+            case OINT_LED_GREEN_S7:
+            case OINT_LED_GREEN_S8:
+                s3c_gpio_setpull(oint_gpios[i], S3C_GPIO_PULL_UP);
+                gpio_direction_output(oint_gpios[i], LED_OFF);
+                break;
+            case OINT_CURR_SWITCH:
+                s3c_gpio_setpull(oint_gpios[i], S3C_GPIO_PULL_UP);
+                gpio_direction_output(oint_gpios[i], CURR_SWITCH_OFF);
+                break;
+            case OINT_FPGA_RESET:
+                s3c_gpio_setpull(oint_gpios[i], S3C_GPIO_PULL_UP);
+                gpio_direction_output(oint_gpios[i], FPGA_RESET_DISABLE);
+                break;
+            case OINT_FPGA_SDRAM_STATUS:
+                s3c_gpio_setpull(oint_gpios[i], S3C_GPIO_PULL_NONE);
+                gpio_direction_input(oint_gpios[i]);
+                break;
+            default:
+                break;
+        }
+    }
+
+    /* register device */
+    ret = misc_register(&oint_gpio_dev);
+
+    printk("init OINT gpio driver");
+
+    return ret;
+}
+
+static void __exit oint_gpio_cleanup(void)
+{
+    int i; 
+
+    for (i = 0; i < GPIO_NUM; i++)
+    {
+        gpio_free(oint_gpios[i]);
+    }
+
+    misc_deregister(&oint_gpio_dev);
+
+    printk("cleanup OINT gpio driver");
+}
+
+module_init(oint_gpio_init);
+module_exit(oint_gpio_cleanup);
+
+MODULE_LICENSE("Dual BSD/GPL");
+MODULE_AUTHOR("Joe Shang");
+MODULE_DESCRIPTION("The gpio driver for OINT");
---
+++ drivers/gpio/oint_gpio.h
@@ -0,0 +1,89 @@
+/**
+ * @file oint_gpio.h
+ * @brief 
+ * @author Joe Shang <shangchuanren@gmail.com>
+ * @version 
+ * @date 2014-05-02
+ */
+
+#ifndef __OINT_GPIO_H_
+#define __OINT_GPIO_H_
+
+/*
+ * LED灯（输出）
+ *  低电平--LED灯亮
+ *  高电平--LED灯灭
+ */
+#define LED_ON                  0
+#define LED_OFF                 1 
+
+/*
+ * 电流控制采样开关（输出）
+ *  低电平--关闭
+ *  高电平--打开
+ */
+#define CURR_SWITCH_OFF         0
+#define CURR_SWITCH_ON          1
+
+/*
+ * FPGA复位开关（输出）
+ *  低电平--FPGA复位
+ *  高电平--FPGA正常工作
+ */
+#define FPGA_RESET_ENABLE       0
+#define FPGA_RESET_DISABLE      1
+
+/*
+ * FPGA的SDRAM状态（输入）
+ *  低电平--FPGA的SDRAM有数据
+ *  高电平--FPGA的SDRAM为空
+ */
+#define FPGA_RESET_ENABLE       0
+#define FPGA_RESET_DISABLE      1
+
+enum _GpioCommand
+{
+    OINT_LED_RED_S1,
+    OINT_LED_RED_S2,
+    OINT_LED_RED_S3,
+    OINT_LED_RED_S4,
+    OINT_LED_RED_S5,
+    OINT_LED_RED_S6,
+    OINT_LED_RED_S7,
+    OINT_LED_RED_S8,
+    OINT_LED_GREEN_S1,
+    OINT_LED_GREEN_S2,
+    OINT_LED_GREEN_S3,
+    OINT_LED_GREEN_S4,
+    OINT_LED_GREEN_S5,
+    OINT_LED_GREEN_S6,
+    OINT_LED_GREEN_S7,
+    OINT_LED_GREEN_S8,
+    OINT_CURR_SWITCH,
+    OINT_FPGA_RESET,
+    OINT_FPGA_SDRAM_STATUS
+};
+
+/* ioctl命令 */
+#define OINT_GPIO_IOC_MAGIC                 'o'
+#define OINT_GPIO_IOC_LED_RED_S1            _IOW(OINT_GPIO_IOC_MAGIC, OINT_LED_RED_S1, int)
+#define OINT_GPIO_IOC_LED_RED_S2            _IOW(OINT_GPIO_IOC_MAGIC, OINT_LED_RED_S2, int)
+#define OINT_GPIO_IOC_LED_RED_S3            _IOW(OINT_GPIO_IOC_MAGIC, OINT_LED_RED_S3, int)
+#define OINT_GPIO_IOC_LED_RED_S4            _IOW(OINT_GPIO_IOC_MAGIC, OINT_LED_RED_S4, int)
+#define OINT_GPIO_IOC_LED_RED_S5            _IOW(OINT_GPIO_IOC_MAGIC, OINT_LED_RED_S5, int)
+#define OINT_GPIO_IOC_LED_RED_S6            _IOW(OINT_GPIO_IOC_MAGIC, OINT_LED_RED_S6, int)
+#define OINT_GPIO_IOC_LED_RED_S7            _IOW(OINT_GPIO_IOC_MAGIC, OINT_LED_RED_S7, int)
+#define OINT_GPIO_IOC_LED_RED_S8            _IOW(OINT_GPIO_IOC_MAGIC, OINT_LED_RED_S8, int)
+#define OINT_GPIO_IOC_LED_GREEN_S1          _IOW(OINT_GPIO_IOC_MAGIC, OINT_LED_GREEN_S1, int)
+#define OINT_GPIO_IOC_LED_GREEN_S2          _IOW(OINT_GPIO_IOC_MAGIC, OINT_LED_GREEN_S2, int)
+#define OINT_GPIO_IOC_LED_GREEN_S3          _IOW(OINT_GPIO_IOC_MAGIC, OINT_LED_GREEN_S3, int)
+#define OINT_GPIO_IOC_LED_GREEN_S4          _IOW(OINT_GPIO_IOC_MAGIC, OINT_LED_GREEN_S4, int)
+#define OINT_GPIO_IOC_LED_GREEN_S5          _IOW(OINT_GPIO_IOC_MAGIC, OINT_LED_GREEN_S5, int)
+#define OINT_GPIO_IOC_LED_GREEN_S6          _IOW(OINT_GPIO_IOC_MAGIC, OINT_LED_GREEN_S6, int)
+#define OINT_GPIO_IOC_LED_GREEN_S7          _IOW(OINT_GPIO_IOC_MAGIC, OINT_LED_GREEN_S7, int)
+#define OINT_GPIO_IOC_LED_GREEN_S8          _IOW(OINT_GPIO_IOC_MAGIC, OINT_LED_GREEN_S8, int)
+#define OINT_GPIO_IOC_CURR_SWITCH           _IOW(OINT_GPIO_IOC_MAGIC, OINT_CURR_SWITCH, int)
+#define OINT_GPIO_IOC_FPGA_RESET            _IOW(OINT_GPIO_IOC_MAGIC, OINT_FPGA_RESET, int)
+#define OINT_GPIO_IOC_FPGA_SDRAM_STATUS     _IOR(OINT_GPIO_IOC_MAGIC, OINT_FPGA_SDRAM_STATUS, int)
+
+#endif
