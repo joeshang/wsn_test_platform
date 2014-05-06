186fde50a436c06486d601b9adeefa4b9a42a114
joeshang <shangchuanren@gmail.com>
2014-05-06 10:32:32 +0800

    [Feature] 完成app的README，以及对应Makefile框架

---
+++ app/main.c
@@ -0,0 +1,14 @@
+/**
+ * @file main.c
+ * @brief 
+ * @author Joe Shang <shangchuanren@gmail.com>
+ * @version 
+ * @date 2014-05-04
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
